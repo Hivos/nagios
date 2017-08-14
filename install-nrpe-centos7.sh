c7f72a9df8ff5515f2cfe528c28b676b64942796
Barry de Graaff <info@barrydegraaff.tk>
2017-08-14 10:45:14 +0200

    Update install-nrpe-centos7.sh

--- install-nrpe-centos7.sh
+++ install-nrpe-centos7.sh
@@ -8,7 +8,7 @@ yum install -y nrpe nagios-plugins-users nagios-plugins-load nagios-plugins-swap
 sed -i 's|^allowed_hosts=127.0.0.1|allowed_hosts=127.0.0.1,192.168.200.29|' /etc/nagios/nrpe.cfg
 
 #Configure the firewall to allow nrpe traffic
-firewall-cmd --permanent --add-port=5666/udp
+firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="192.168.200.29/22" port protocol="udp" port="5666" accept'
 firewall-cmd --reload
 
 #Restart NRPE and make sure that NRPE is started at boot
