67c41c91860d8cefa4cd7fc17326bf01f47d3250
nmlgomes <31000237+nmlgomes@users.noreply.github.com>
2017-08-14 15:11:52 +0200

    Update install-nrpe-centos7.sh

--- install-nrpe-centos7.sh
+++ install-nrpe-centos7.sh
@@ -1,16 +1,26 @@
-# Add the EPEL repository
-yum install -y epel-release
+#Define conditional expression
+YUM_CMD=$(which yum)
 
-# Install NRPE and required plugins
-yum install -y nrpe nagios-plugins-users nagios-plugins-load nagios-plugins-swap nagios-plugins-disk nagios-plugins-procs
+#Check package manager, add the EPEL repository and install NRPE and required plugins
+if [[ ! -z $YUM_CMD ]]; then
+   yum install -y epel-release
+   yum install -y nrpe nagios-plugins-users nagios-plugins-load nagios-plugins-swap nagios-plugins-disk nagios-plugins-procs
+else
+   apt-get install -y nagios-nrpe-server nagios-plugins
+fi
 
-#Add your Monitor server(s) address(es) to the allowed_hosts sections. 
+#Add the nagios server(s) address(es) to the allowed_hosts sections. 
 sed -i 's|^allowed_hosts=127.0.0.1|allowed_hosts=127.0.0.1,192.168.200.29|' /etc/nagios/nrpe.cfg
 
-#Configure the firewall to allow nrpe traffic
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="192.168.200.29/22" port protocol="udp" port="5666" accept'
-firewall-cmd --reload
+#Configure the firewall to allow nrpe traffic, restart NRPE and make sure that NRPE is started at boot
+echo "If Debian, manually add exception for tcp port 5666 in iptables"
+if [[ ! -z $YUM_CMD ]]; then
+    firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="192.168.200.29/22" port protocol="udp" port="5666$
+    firewall-cmd --reload
+    
+    systemctl restart nrpe
+    systemctl enable nrpe
+else
+    service nagios-nrpe-server restart
+fi
 
-#Restart NRPE and make sure that NRPE is started at boot
-systemctl restart nrpe
-systemctl enable nrpe
