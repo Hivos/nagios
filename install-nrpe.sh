2a1bc1c82021a34b75aac3ed710b985cfa1be3cf
Barry de Graaff <info@barrydegraaff.tk>
2017-08-17 09:53:55 +0200

    Update install-nrpe.sh

--- install-nrpe.sh
+++ install-nrpe.sh
@@ -13,6 +13,12 @@ fi
 #Add the nagios server(s) address(es) to the allowed_hosts sections. 
 sed -i 's|^allowed_hosts=127.0.0.1|allowed_hosts=127.0.0.1,192.168.200.29|' /etc/nagios/nrpe.cfg
 
+#Add bandwidth check
+wget https://raw.githubusercontent.com/Hivos/nagios-plugins/master/check_bandwidth.sh -O /usr/local/sbin/check_bandwidth.sh
+chmod +rx /usr/local/sbin/check_bandwidth.sh
+sed -i "s^eth0^$(ip a|grep 192. -B2|grep mtu|grep -v DOWN|awk '{print $2}'|sed 's/://g')^" /usr/local/sbin/check_bandwidth.sh
+echo command[check_bandwidth]=/usr/local/sbin/check_bandwidth.sh >> /etc/nagios/nrpe.cfg
+
 #Configure the firewall to allow nrpe traffic, restart NRPE and make sure that NRPE is started at boot
 if [[ ! -z $YUM_CMD ]]; then
     firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="192.168.200.29/22" port protocol="udp" port="5666" accept'
